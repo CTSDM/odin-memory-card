f6f70354da8f187fdd8b8a53ec94bf7df52a2686
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-12-20 17:03:06 +0900

    First commit

---
+++ config/images.js
@@ -0,0 +1,10 @@
+const imagesUrl = {
+    diablos:
+        "https://static.wikia.nocookie.net/monsterhunter/images/3/39/MHRise-Diablos_Render_001.png",
+    deviljho:
+        "https://static.wikia.nocookie.net/monsterhunter/images/c/cb/MHW-Deviljho_Render_001.png",
+    bazelgeuse:
+        "https://static.wikia.nocookie.net/monsterhunter/images/3/3c/MH20th-Bazelgeuse_Render_001.png",
+};
+
+export default imagesUrl;
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import "./components/Card.jsx";
+
+function App() {
+    return <></>;
+}
+
+export default App;
